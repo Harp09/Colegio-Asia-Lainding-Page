69fa41c6ed1085ada5d200a384595698d921c49a
Harp09 <hebert.rivera@vallegrande.edu.pe>
2022-12-13 05:15:01 -0500

    dfaj

---
+++ server.js
@@ -0,0 +1,14 @@
+const http = require('http');
+
+//const server = http.createServer((req,res) => {
+//    res.end('Hello word')
+//})
+app.get('/', (req, res) => {
+res.sendFile("/home/ubuntu/Colegio-Asia-Lainding-Page/index.html")
+});
+
+//Recursos
+app.use(express.static(__dirname+'/'));
+
+server.listen(3000);
+console.log('server on port 3000');
